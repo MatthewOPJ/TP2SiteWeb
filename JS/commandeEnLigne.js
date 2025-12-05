4aba4197f1a32c799ce75ee45da0f18defaa5d03
Matthew <m.philibert.jones@gmail.com>
2025-12-05 16:57:46 -0500

    Saveur fontionnelle

--- JS/commandeEnLigne.js
+++ JS/commandeEnLigne.js
@@ -1,17 +1,17 @@
 function changeImage() {
     // Get the select elements and the image element
-    var saveur = document.getElementById("saveur");
-    var glacage = document.getElementById("glacage");
+    var saveurDeBaseChoisie = document.getElementById("saveurDeBaseChoisie");
+    var saveurDeGlacageChoisie = document.getElementById("glacage");
     var image = document.getElementById("changeImage");
 
     // Get the selected values
-    var valeurSaveur = saveur.value;
-    var valeurGlacage = glacage.value;
+    var valeurSaveur = saveurDeBaseChoisie.value;
+    //var valeurGlacage = saveurDeGlacageChoisie.value;
 
     // Construct the new image filename/path
     // This assumes your images are named consistently, e.g., "images/red_square.png"
     var newImageSrc = "../images/base_" + valeurSaveur + ".png";
-    var imgGlacage = "../images/base_" + valeurSaveur + "_glacage_" + valeurGlacage + ".png";
+    //var imgGlacage = "../images/base_" + valeurSaveur + "_glacage_" + valeurGlacage + ".png";
 
     image.src = newImageSrc;
 }
